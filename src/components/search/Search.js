ccb798e6657ff7858462bdbad0308e9ee95a3306
Marco-Medhat <eng.marcomedhat@gmail.com>
2019-08-30 16:51:05 +0200

    adjusting Search component to clear results when textfield is empty

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -15,20 +15,25 @@ class Search extends Component {
 	};
 
 	onTextChange = e => {
-		this.setState( { [e.target.name]: e.target.value },
+		const val = e.target.value
+		this.setState( { [e.target.name]: val },
 			() => {
-				axios
-					.get(
-						`${ this.state.apiUrl }/?key=${ this.state.apiKey }
+				if ( val === '' ) {
+					this.setState( { images: [] } )
+				} else {
+					axios
+						.get(
+							`${ this.state.apiUrl }/?key=${ this.state.apiKey }
 					&q=${this.state.searchText }&image_type=photo
 					&per_page=${this.state.amount }&safesearch=true`
-					)
-					.then( res =>
-						this.setState( {
-							images: res.data.hits
-						} )
-					)
-					.catch( err => console.log( err ) );
+						)
+						.then( res =>
+							this.setState( {
+								images: res.data.hits
+							} )
+						)
+						.catch( err => console.log( err ) );
+				}
 			}
 		);
 	};
@@ -39,7 +44,6 @@ class Search extends Component {
 		} );
 
 	render () {
-		console.log( this.state.images );
 		return (
 			<div>
 				<TextField
